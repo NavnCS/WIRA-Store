8aa7968afb197bc9e7d424b2313f309a21a528ac
Navn <navncsofficial@gmail.com>
2025-05-08 08:19:58 +0800

    UI Adjustment

--- frontend/wiraUi/src/App.vue
+++ frontend/wiraUi/src/App.vue
@@ -8,14 +8,14 @@
 
     <main class="nft-gallery">
       <NFTCard
-      v-for="nft in filteredNFTs"
-      :key="nft.id"
-      :id="nft.id"
-      :name="nft.name"
-      :rarity="nft.rarity"
-      :image="nft.image"
-      :is-favorite="nft.isFavorite"
-      @toggle-favorite="toggleFavorite"
+        v-for="nft in filteredNFTs"
+        :key="nft.id"
+        :id="nft.id"
+        :name="nft.name"
+        :rarity="nft.rarity"
+        :image="nft.image"
+        :is-favorite="nft.isFavorite"
+        @toggle-favorite="toggleFavorite"
       />
     </main>
   </div>
@@ -32,41 +32,42 @@ export default {
     FilterBar
   },
   data() {
-  return {
-    selectedRarity: 'All',
-    favorites: [],
-    nfts: [
-      { id: 1, name: 'Crypto Dog', rarity: 'Common', image: '/nft-placeholder.png' },
-      { id: 2, name: 'Pixel Alien', rarity: 'Rare', image: '/nft-placeholder.png' },
-      { id: 3, name: 'Golden Robot', rarity: 'Legendary', image: '/nft-placeholder.png' },
-      { id: 4, name: 'Moon Cat', rarity: 'Common', image: '/nft-placeholder.png' },
-    ]
-  }
-},
-computed: {
-  filteredNFTs() {
-    let list = this.nfts
-    if (this.selectedRarity !== 'All') {
-      list = list.filter(nft => nft.rarity === this.selectedRarity)
+    return {
+      selectedRarity: 'All',
+      favorites: [],
+      nfts: [
+        { id: 1, name: 'Crypto Dog', rarity: 'Common', image: '/nft-placeholder.png' },
+        { id: 2, name: 'Pixel Alien', rarity: 'Rare', image: '/nft-placeholder.png' },
+        { id: 3, name: 'Golden Robot', rarity: 'Legendary', image: '/nft-placeholder.png' },
+        { id: 4, name: 'Moon Cat', rarity: 'Common', image: '/nft-placeholder.png' },
+      ]
     }
-    return list.map(nft => ({
-      ...nft,
-      isFavorite: this.favorites.includes(nft.id)
-    }))
-  }
-},
-methods: {
-  setRarity(rarity) {
-    this.selectedRarity = rarity
   },
-  toggleFavorite(id) {
-    const index = this.favorites.indexOf(id)
-    if (index >= 0) {
-      this.favorites.splice(index, 1)
-    } else {
-      this.favorites.push(id)
+  computed: {
+    filteredNFTs() {
+      let list = this.nfts
+      if (this.selectedRarity !== 'All') {
+        list = list.filter(nft => nft.rarity === this.selectedRarity)
+      }
+      return list.map(nft => ({
+        ...nft,
+        isFavorite: this.favorites.includes(nft.id)
+      }))
+    }
+  },
+  methods: {
+    setRarity(rarity) {
+      this.selectedRarity = rarity
+    },
+    toggleFavorite(id) {
+      const index = this.favorites.indexOf(id)
+      if (index >= 0) {
+        this.favorites.splice(index, 1)
+      } else {
+        this.favorites.push(id)
+      }
     }
   }
-}
 }
 </script>
+
